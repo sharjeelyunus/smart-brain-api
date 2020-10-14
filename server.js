2086d399c036078e4f881e75b78dc839a98c1064
sharjeelyunus <sharjeel924@gmail.com>
2020-10-15 02:16:37 +0500

    🐛 FIX: log

--- server.js
+++ server.js
@@ -54,5 +54,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port 3000 ${process.env.PORT}`);
+    console.log(`app is running on port ${process.env.PORT}`);
 })
